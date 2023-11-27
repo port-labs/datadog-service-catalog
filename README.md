3ca99655b569aa3fa8df3c02b12f7831620c7b60
Pages <isaac.p.coffie@gmail.com>
2023-11-27 10:04:57 +0000

    ingest service catalog to Port

---
+++ README.md
@@ -0,0 +1,78 @@
+# Ingesting Datadog Service Catalog
+
+
+## Getting started
+
+In this example you will create a`datadogServiceCatalog` blueprint that ingests all service catalogs from your Datadog account. You will then add some python script to make API calls to Datadog REST API and fetch data for your account.
+
+## Service Catalog Blueprint
+Create the Monitor blueprint in Port using this schema:
+
+```json 
+{
+  "identifier": "datadogServiceCatalog",
+  "description": "This blueprint represents a Datadog service catalog",
+  "title": "Datadog Service Catalog",
+  "icon": "Datadog",
+  "schema": {
+    "properties": {
+      "contacts": {
+        "type": "array",
+        "title": "Contacts"
+      },
+      "description": {
+        "title": "Description",
+        "type": "string"
+      },
+      "tags": {
+        "type": "array",
+        "items": {
+          "type": "string"
+        },
+        "title": "Tags"
+      },
+      "languages": {
+        "items": {
+          "type": "string"
+        },
+        "title": "Languages",
+        "type": "array"
+      }
+    },
+    "required": []
+  },
+  "mirrorProperties": {},
+  "calculationProperties": {},
+  "relations": {}
+}
+```
+
+## Running the python script
+
+To ingest data from your Datadog account to Port, run the following commands: 
+
+```bash
+export PORT_CLIENT_ID=<ENTER CLIENT ID>
+export PORT_CLIENT_SECRET=<ENTER CLIENT SECRET>
+export DATADOG_API_KEY=<ENTER DATADOG API KEY>
+export DATADOG_APPLICATION_KEY=<ENTER DATADOG APPLICATION KEY>
+export DATADOG_API_URL=<ENTER DATADOG API URL>
+
+git clone https://github.com/port-labs/datadog-service-catalog.git
+
+cd datadog-service-catalog
+
+pip install -r ./requirements.txt
+
+python app.py
+```
+
+The list of variables required to run this script are:
+- `PORT_CLIENT_ID`
+- `PORT_CLIENT_SECRET`
+- `DATADOG_API_KEY`
+- `DATADOG_APPLICATION_KEY`
+- `DATADOG_API_URL`
+
+Please note that by deafult, all Datadog API clients are configured to consume Datadog US site APIs (https://api.datadoghq.com). If you are on the Datadog EU site, set the environment variable `DATADOG_API_URL` to `https://api.datadoghq.eu`. Some Datadog clients may require you to add your account region to the API. In this case, you may change the DATADOG_API_URL to `https://api.<region>.datadoghq.com` or `https://api.<region>.datadoghq.eu`
+
---
+++ app.py
@@ -0,0 +1,73 @@
+## Import the needed libraries
+import requests
+from decouple import config
+from loguru import logger
+
+
+# Get environment variables using the config object or os.environ["KEY"]
+# These are the credentials passed by the variables of your pipeline to your tasks and in to your env
+
+PORT_CLIENT_ID = config("PORT_CLIENT_ID")
+PORT_CLIENT_SECRET = config("PORT_CLIENT_SECRET")
+DATADOG_API_KEY = config("DATADOG_API_KEY")
+DATADOG_APPLICATION_KEY = config("DATADOG_APPLICATION_KEY")
+DATADOG_API_URL = config("DATADOG_API_URL")
+PORT_API_URL = "https://api.getport.io/v1"
+
+
+## Get Port Access Token
+credentials = {'clientId': PORT_CLIENT_ID, 'clientSecret': PORT_CLIENT_SECRET}
+token_response = requests.post(f'{PORT_API_URL}/auth/access_token', json=credentials)
+access_token = token_response.json()['accessToken']
+
+# You can now use the value in access_token when making further requests
+headers = {
+	'Authorization': f'Bearer {access_token}'
+}
+blueprint_id = 'datadogServiceCatalog'
+
+
+def add_entity_to_port(entity_object):
+    """A function to create the passed entity in Port
+
+    Params
+    --------------
+    entity_object: dict
+        The entity to add in your Port catalog
+    
+    Returns
+    --------------
+    response: dict
+        The response object after calling the webhook
+    """
+    response = requests.post(f'{PORT_API_URL}/blueprints/{blueprint_id}/entities?upsert=true&merge=true', json=entity_object, headers=headers)
+    logger.info(response.json())
+
+
+def retrieve_service_catalogs():
+    """A function to make API request to Datadog to retrieve Service catalogs"""
+
+    logger.info("fetching service catalog data")
+    datadog_headers = {'DD-API-KEY': f'{DATADOG_API_KEY}', 'DD-APPLICATION-KEY': f'{DATADOG_APPLICATION_KEY}', 'Accept': 'application/json'}
+    datadog_response = requests.get(f'{DATADOG_API_URL}/api/v2/services/definitions', headers=datadog_headers)
+    datadog_response.raise_for_status()
+    logger.info("successfully retrieved service catalog data")
+    services = datadog_response.json().get("data", [])
+
+    for service in services:
+        schema_data = service["attributes"]["schema"]
+        entity = {
+            "identifier": schema_data["dd-service"],
+            "title": schema_data["dd-service"],
+            "properties": {
+                "contacts": schema_data.get("contacts", []),
+                "languages": schema_data.get("languages", []),
+                "description": schema_data.get("description"),
+                "tags": schema_data.get("tags", [])
+            },
+            "relations": {}
+            }
+        add_entity_to_port(entity)
+
+if __name__ == "__main__":
+    retrieve_service_catalogs()
